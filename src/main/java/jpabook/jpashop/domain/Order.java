1bf72ad05405acd0190c1f41003d7407e2d7f75a
yunhwan <yhyoon1004@naver.com>
2024-01-16 22:12:14 +0900

    엔티티 설계시 주의점 --- @nToOne의 연관관계의 경우 persist 전략이 즉시로딩(EAGER)으로 되어있어 나중에 문제발생시 찾기가 힘듬. 그래서 지연로딩으로 변경하여 해당 외래키 객체의 데이터가 사용될 때 DB쿼리가 나가게 설정해야됨 --- @Setter를 사용하는 것을 지양하는 이유 jpa하이버네이트는 컬랙션 필드를 하이버네이트가 관리하는 컬렉션에 넣는 데, setter를 사용하면 하이버네이트가 설정한 값이 문제가 생김 --- 연관관계의 필드의 경우 cascade 옵션으로 같이 한꺼번에 persist하도록 옵션을 주고, 연관관계 편의 메서드로 연관관계의 필드값을 넣어 코드를 가시적이고 깔끔하게 하자

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -9,6 +9,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter@Setter
@@ -16,17 +18,51 @@ public class Order {
     @Id@GeneratedValue(strategy = GenerationType.IDENTITY )
     @Column(name = "order_id")
     private Long id;
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
+    /* 설명
+    * orderItem이 여러개면 아래와 같이 각각 persist()에 넣어 주고 엔티티를 persist 해야하는데
+    * persist(orderItemA);
+    * persist(orderItemB);
+    * persist(orderItemC);
+    * persist(order);
+    * vvvvvvvvv cascade 옵션을 주면
+    * persist(order);   위 코드를 한번에 해결
+    * CascadeType.ALL로 해놓았을 경우 delete 할때도 전부 제거
+    * */
 
-    @OneToOne
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    //위의 cascade도 연관된 엔티티도 같이 persist해줌
+    //entity는 원래 각각 persist()해줘야함, 하지만 cascade 옵션으로 함께 처리가능
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
     private LocalDateTime orderDate;
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+
+    //연관관계(편의) 메서드 -> 컨트롤하는 엔티티쪽이 가지고 있는 것이 좋음
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+//    _위의 메서드가 없을 경우 코드_
+//    member.getOrders().add(order);
+//    order.setMember(member);
+//    ------위 처럼 처리해야하는 코드가 연관관계편의 메서드 사용시--
+//    order.setMember(member); 이렇게 깔끔해, 양방향 연관관계 설정해줌
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
