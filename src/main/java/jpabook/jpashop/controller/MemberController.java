497f75bd74210d4dedbb29b080dcbccfcedc7938
yunhwan <yhyoon1004@naver.com>
2024-01-30 20:53:08 +0900

    회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -3,6 +3,7 @@ package jpabook.jpashop.controller;
 import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.bind.BindResult;
 import org.springframework.stereotype.Controller;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,7 +30,7 @@ public class MemberController {
     @PostMapping("/members/new")
     //@Valid -> 해당 객체에 있는 vaildate 어노테이션이 실행되기 위한 @Vaild 어노테이션
     //BindingResult -> 오류 발생시 해당 오류를 BindResult에 담은체로 메서드 실행
-    public String create(@Valid MemberForm form, BindingResult result) {
+    public String create(@Valid MemberForm form, BindingResult result) {        //회원가입
 
         if (result.hasErrors()) {
             return "members/createMemberForm";
@@ -42,5 +44,12 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {       //회원 목록
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
