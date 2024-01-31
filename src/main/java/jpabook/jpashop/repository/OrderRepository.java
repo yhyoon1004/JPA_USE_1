5fd514a94c6ba5322bca71f57fb23bcd6e4e4b1a
yunhwan <yhyoon1004@naver.com>
2024-01-31 21:59:42 +0900

    주문 목록 검색, 취소

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,13 +21,14 @@ public class OrderRepository {
     }
 
     public List<Order> findAll(OrderSearch orderSearch) {
+        System.out.println("orderSearch = name: " + orderSearch.getMemberName()+" / status :  "+orderSearch.getOrderStatus());
         return em.createQuery("select o from Order o join o.member  m " +
                         "where o.status = :status " +
                         "and m.name like :name", Order.class)
                 .setParameter("status", orderSearch.getOrderStatus())
                 .setParameter("name", orderSearch.getMemberName())
 //                .setFirstResult(100)//페이징할 때 사용되는 메서드 100(넣어준 값) 부터 결과를 가져옴
-                .setMaxResults(1000 )//조회된 결과를 최대 1000(넣어준 값) 개 까지 받아오는 메서드
+                .setMaxResults(1000)//조회된 결과를 최대 1000(넣어준 값) 개 까지 받아오는 메서드
                 .getResultList();
 
     }
