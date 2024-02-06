ce29493ab19a5467b76b43a023a410ff90e6adee
yunhwan <yhyoon1004@naver.com>
2024-02-06 19:20:12 +0900

    주문 조회 v3.1 : 엔티티를 DTO 변환 - 페이징과 한계 돌파

--- src/main/java/jpabook/jpashop/api/OrderAPIController.java
+++ src/main/java/jpabook/jpashop/api/OrderAPIController.java
@@ -10,6 +10,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -48,6 +49,17 @@ public class OrderAPIController {
         List<Order> orders =orderRepository.findAllWithItem();
         List<OrderDTO> collect = orders.stream()
                 .map(o -> new OrderDTO(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDTO> ordersV3_page(@RequestParam(value = "offset",defaultValue = "0") int offset,
+                                        @RequestParam(value = "limit",defaultValue = "100") int limit
+                                        ) {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        List<OrderDTO> collect = orders.stream()
+                .map(o-> new OrderDTO(o))
                 .collect(Collectors.toList());
         return collect;
     }
